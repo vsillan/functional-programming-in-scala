f96da6560cfaa74a03a6f4553a0de4035cdd1af8
Ville Sillanpää <ville.sillanpaa@futurice.com>
2021-03-28 19:47:50 +0300

    Update MyEither to use MyList

--- src/main/scala/MyEither.scala
+++ src/main/scala/MyEither.scala
@@ -1,56 +1,58 @@
-sealed trait MyEither[+E, +A] {
-  def map[B](f: A => B): MyEither[E, B] = {
-    this match {
-      case Right(a) => Right(f(a))
-      case Left(e)  => Left(e)
+package scala_book {
+  sealed trait MyEither[+E, +A] {
+    def map[B](f: A => B): MyEither[E, B] = {
+      this match {
+        case Right(a) => Right(f(a))
+        case Left(e)  => Left(e)
+      }
+    }
+    def flatMap[EE >: E, B](f: A => MyEither[EE, B]): MyEither[EE, B] = {
+      this match {
+        case Right(a) => f(a)
+        case Left(e)  => Left(e)
+      }
+    }
+
+    def orElse[EE >: E, AA >: A](b: => MyEither[EE, AA]): MyEither[EE, AA] = {
+      this match {
+        case Right(a) => Right(a)
+        case Left(e)  => b
+      }
+    }
+
+    def map2[EE >: E, B, C](
+        b: MyEither[EE, B]
+    )(f: (A, B) => C): MyEither[EE, C] = {
+      this match {
+        case Right(a) => this.flatMap(aa => b.map(bb => f(aa, bb)))
+        case Left(e)  => Left(e)
+      }
+    }
+
+    def map2Alt[EE >: E, B, C](
+        b: MyEither[EE, B]
+    )(f: (A, B) => C): MyEither[EE, C] = {
+      this match {
+        case Right(a) => this.flatMap(aa => b.map(bb => f(aa, bb)))
+        case Left(e)  => Left(e)
+      }
     }
   }
-  def flatMap[EE >: E, B](f: A => MyEither[EE, B]): MyEither[EE, B] = {
-    this match {
-      case Right(a) => f(a)
-      case Left(e)  => Left(e)
+  case class Left[+E](value: E) extends MyEither[E, Nothing]
+  case class Right[+A](value: A) extends MyEither[Nothing, A]
+
+  object MyEither {
+    def sequence[E, A](es: MyList[MyEither[E, A]]): MyEither[E, MyList[A]] = {
+      traverse(es)(x => x)
     }
-  }
 
-  def orElse[EE >: E, AA >: A](b: => MyEither[EE, AA]): MyEither[EE, AA] = {
-    this match {
-      case Right(a) => Right(a)
-      case Left(e)  => b
-    }
-  }
-
-  def map2[EE >: E, B, C](
-      b: MyEither[EE, B]
-  )(f: (A, B) => C): MyEither[EE, C] = {
-    this match {
-      case Right(a) => this.flatMap(aa => b.map(bb => f(aa, bb)))
-      case Left(e)  => Left(e)
-    }
-  }
-
-  def map2Alt[EE >: E, B, C](
-      b: MyEither[EE, B]
-  )(f: (A, B) => C): MyEither[EE, C] = {
-    this match {
-      case Right(a) => this.flatMap(aa => b.map(bb => f(aa, bb)))
-      case Left(e)  => Left(e)
-    }
-  }
-}
-case class Left[+E](value: E) extends MyEither[E, Nothing]
-case class Right[+A](value: A) extends MyEither[Nothing, A]
-
-object MyEither {
-  def sequence[E, A](es: List[MyEither[E, A]]): MyEither[E, List[A]] = {
-    traverse(es)(x => x)
-  }
-
-  def traverse[E, A, B](
-      as: List[A]
-  )(f: A => MyEither[E, B]): MyEither[E, List[B]] = {
-    as match {
-      case Nil    => Right(Nil)
-      case h :: t => (f(h) map2 traverse(t)(f))(_ :: _)
+    def traverse[E, A, B](
+        as: MyList[A]
+    )(f: A => MyEither[E, B]): MyEither[E, MyList[B]] = {
+      as match {
+        case Nil        => Right(Nil)
+        case Cons(h, t) => (f(h) map2 traverse(t)(f))(_ :: _)
+      }
     }
   }
 }
