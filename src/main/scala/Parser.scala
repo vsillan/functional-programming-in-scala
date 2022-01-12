85ea0695a99cea27c3bda537f4a9e064194a7272
Ville Sillanpää <ville.sillanpaa@futurice.com>
2022-01-12 15:06:56 +0200

    First definition of Parser

---
+++ src/main/scala/Parser.scala
@@ -0,0 +1,24 @@
+package scala_book {
+  trait Parsers[ParseError, Parser[+_]] { self =>
+    def run[A](p: Parser[A])(input: String): Either[ParseError, A]
+
+    def char(c: Char): Parser[Char]
+
+    implicit def string(s: String): Parser[String]
+
+    implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
+
+    implicit def asStringParser[A](a: A)(implicit
+        f: A => Parser[String]
+    ): ParserOps[String] = ParserOps(f(a))
+
+    def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+
+    def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
+
+    case class ParserOps[A](p: Parser[A]) {
+      def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+      def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
+    }
+  }
+}
