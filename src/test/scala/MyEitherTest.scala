6d17dce6b1c129e9175faecc2d045e0d1be02a85
Ville Sillanpää <ville.sillanpaa@futurice.com>
2022-01-20 12:09:05 +0200

    Fix test

--- src/test/scala/MyEitherTest.scala
+++ src/test/scala/MyEitherTest.scala
@@ -2,10 +2,10 @@ import scala_book._
 
 class MyEitherTest extends UnitSpec {
   "flatMap" should "allow to map inner value to new either" in {
-    assert(Right(3).flatMap(a => Right(a * 2)) == Right(6))
+    assert(MyRight(3).flatMap(a => MyRight(a * 2)) == MyRight(6))
   }
 
   "map2" should "allow combining two inner values" in {
-    assert(Right(2).map2(Right(3))((a, b) => a * b) == Right(6))
+    assert(MyRight(2).map2(MyRight(3))((a, b) => a * b) == MyRight(6))
   }
 }
